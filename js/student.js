b7e87bfb1cef0b6bc5220e3d1a6b3fc8a5f376ab
Tcreavis <54606651+Tcreavis@users.noreply.github.com>
2019-10-19 17:44:34 -0500

    Deletion Works!

--- js/student.js
+++ js/student.js
@@ -7,8 +7,10 @@ function Student(id, first, last, age, degree){
     this.degree = degree;
 }
 
-findStudents();
+//Variable to hold student objects.
+var newStudent;
 
+//Array to hold all student objects.
 var studentArr = [];
 
 //Variables to hold each of our input objects
@@ -25,34 +27,36 @@ var studentLast;
 var studentAge;
 var studentDegree;
 
-//Variable to hold student object.
-var newStudent;
+//Get the most recent list of students from storage.
+refreshStudents();
 
 //Select the submit button, and listen for a click.
 document.getElementById("submitBut").addEventListener("click", function() {
 
-    //Define each variable with their respective user inputs. This step is just for easier reading.
+    //Initialize each variable with their respective user inputs.
     studentID = idInput.value;
     studentFirst = firstInput.value;
     studentLast = lastInput.value;
     studentAge = ageInput.value;
     studentDegree = degreeInput.value;
 
+    //A boolean to flag if any input fields were left blank.
     var anyBlanks = false;
 
+    //An array containing the values of each input element. Used only for input validation.
     var studentElements = [studentID, studentFirst, studentLast, studentAge, studentDegree];
 
-    for(var i = 0; i < 4; i++){
+    //If any of the values are empty, set the boolean to true.
+    for(var i = 0; i < studentElements.length; i++){
         if(!studentElements[i]){
             anyBlanks = true;
         }
     } 
 
-    console.log(anyBlanks);
-
+    //As long as we didn't find any blanks, the following code will execute.
     if(!anyBlanks){
 
-        //Create a new student object with this information.
+        //Create a new student object with the values from the input fields.
         newStudent = new Student(
         studentID, 
         studentFirst, 
@@ -67,46 +71,138 @@ document.getElementById("submitBut").addEventListener("click", function() {
         ageInput.value = "";
         degreeInput.value = "";
 
+        if(window.localStorage.getItem("studentArr")){
+
+            studentArr = JSON.parse(window.localStorage.getItem("studentArr"));
+        
+        }
+
+        //Append the new student to the student array.
         studentArr.push(newStudent);
 
-        //Store this newly created object.
-        window.localStorage.setItem("student", JSON.stringify(newStudent));
+        //Add the new student array to local storage, or overwrite the old one.
+        //Local storage requires objects and arrays to be "stringified."
+        //We can parse them out when we need to.
+        window.localStorage.setItem("studentArr", JSON.stringify(studentArr));
 
-        findStudents();
-
+        //Add the new student to the page.
+        refreshStudents();
     }
 
+    //Let the user know they left something blank.
     else{
         alert("You left something blank!");
     }
 
 })
 
-function findStudents(){
+function giveDeleteFunction(){
 
-    if(window.localStorage.getItem("student") != null){
+    var delButtons = document.getElementsByClassName("delButton");
 
-        var student = JSON.parse(window.localStorage.getItem('student'));
-    
-        var list = document.querySelector("ul");
-        var entry = document.createElement("li");
-        var text = document.createTextNode("Name: " + student.firstName + " " + student.lastName + ", ID: " + student.id + ", Age: " + student.age + ", Major:" + student.degree)
-        entry.appendChild(text);
-        list.appendChild(entry);
+    for(var x = 0; x < delButtons.length; x++){
 
+        delButtons[x].addEventListener("click", function(){
+            
+            deleteStudent(this.id);
+
+        });
     }
 }
 
+function deleteStudent(id){
+
+    var copyStudentArr = JSON.parse(window.localStorage.getItem("studentArr"));
+
+    var deletionID = id;
+    var checkThisID;
+    var deletionIndex = 99;
+
+    for(var x = 0; x < copyStudentArr.length; x++){
+
+        checkThisID = copyStudentArr[x].id;
+
+        if(deletionID == checkThisID){
+
+            deletionIndex = x;
+
+        }
+
+    }
+
+    copyStudentArr.splice(deletionIndex, deletionIndex+1);
+
+    window.localStorage.setItem("studentArr", JSON.stringify(copyStudentArr));
+
+    refreshStudents();
+
+}
+
+
+function refreshStudents(){
+
+    //Erase the current student list.
+    if(document.getElementById("studentlist")){
+
+        document.getElementById("studentlist").innerHTML = "";
+
+    }
+
+    //Check if there is a student array in storage.
+    if(window.localStorage.getItem("studentArr")){
+        
+        //If there is an array of students, pull it from storage, parse the data, and place it in a variable.
+        var getStudentArr = JSON.parse(window.localStorage.getItem("studentArr"));
+        
+        //Go through each object in the array, and use its data to create a new list item.
+        for(var i = 0; i < getStudentArr.length; i++){
+            
+            //Select the students 'ul' and set it to a variable.
+            var list = document.querySelector("ul");
+            //Create a new 'li' element.
+            var entry = document.createElement("li");
+            //Giving all 'li' elements a common attribute for bulk modification.
+            entry.className = "StudentInfo";
+
+            //Create a delete button for each 'li'
+            var delButton = document.createElement("button");
+            //Again, giving all buttons a common attribute for possible convenience later.
+            delButton.className = "delButton";
+            //Giving all buttons a unique ID that mirrors the Student ID with which they are associated.
+            delButton.id = getStudentArr[i].id;
+            //Give the button a label.
+            delButton.innerHTML = "Delete";
+
+            //Create a delete button for each 'li'
+            var editButton = document.createElement("button");
+            //Again, giving all buttons a common attribute for possible convenience later.
+            editButton.className = "editButton";
+            //Giving all buttons a unique ID that mirrors the Student ID with which they are associated.
+            editButton.id = getStudentArr[i].id;
+            //Give the button a label.
+            editButton.innerHTML = "Edit";
 
 
 
+            //Create a string for each student that lists all of their student info 
+            var text = document.createTextNode("Name: " + getStudentArr[i].firstName + " " + getStudentArr[i].lastName 
+                                                + ", ID: " + getStudentArr[i].id 
+                                                + ", Age: " + getStudentArr[i].age 
+                                                + ", Major:" + getStudentArr[i].degree + " ")
 
+            //Give each text node a unique ID as well.
+            text.iD = getStudentArr[i].id;
 
+            //Append the text and delete button to the 'li'
+            entry.appendChild(text);
+            entry.appendChild(delButton);
+            //Append the 'li' to the 'ul'
+            list.appendChild(entry);
 
+        }
 
+        giveDeleteFunction();
 
- 
+    }
 
-
-
-
+}
