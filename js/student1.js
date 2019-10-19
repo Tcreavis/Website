c34dadad9a40fef3bcbf8cd816cd25b7229df8e4
Tcreavis <54606651+Tcreavis@users.noreply.github.com>
2019-10-19 13:30:15 -0500

    Buggy

--- js/student1.js
+++ js/student1.js
@@ -7,9 +7,21 @@ function Student(id, first, last, age, degree){
     this.degree = degree;
 }
 
-findStudents();
+//Variable to hold student objects.
+var newStudent;
 
+//Array to hold all student objects.
 var studentArr = [];
+
+//If there's already an array of student objects in storage, initilize the studentArr with it.
+if(window.localStorage.getItem("studentArr")){
+
+    studentArr = JSON.parse(window.localStorage.getItem("studentArr"));
+
+}
+
+//Inspects the student array and adds all student objects to the page.
+populateStudents();
 
 //Variables to hold each of our input objects
 var idInput = document.getElementById("studID");
@@ -25,34 +37,33 @@ var studentLast;
 var studentAge;
 var studentDegree;
 
-//Variable to hold student object.
-var newStudent;
-
 //Select the submit button, and listen for a click.
 document.getElementById("submitBut").addEventListener("click", function() {
 
-    //Define each variable with their respective user inputs. This step is just for easier reading.
+    //Initialize each variable with their respective user inputs.
     studentID = idInput.value;
     studentFirst = firstInput.value;
     studentLast = lastInput.value;
     studentAge = ageInput.value;
     studentDegree = degreeInput.value;
 
+    //A boolean to flag if any input fields were left blank.
     var anyBlanks = false;
 
+    //An array containing the values of each input element. Used only for input validation.
     var studentElements = [studentID, studentFirst, studentLast, studentAge, studentDegree];
 
+    //If any of the values are empty, set the boolean to true.
     for(var i = 0; i < 4; i++){
         if(!studentElements[i]){
             anyBlanks = true;
         }
     } 
 
-    console.log(anyBlanks);
-
+    //As long as we didn't find any blanks, the following code will execute.
     if(!anyBlanks){
 
-        //Create a new student object with this information.
+        //Create a new student object with the values from the input fields.
         newStudent = new Student(
         studentID, 
         studentFirst, 
@@ -67,32 +78,149 @@ document.getElementById("submitBut").addEventListener("click", function() {
         ageInput.value = "";
         degreeInput.value = "";
 
+        //Append the new student to the student array.
         studentArr.push(newStudent);
 
-        //Store this newly created object.
-        window.localStorage.setItem("student", JSON.stringify(newStudent));
+        //Add the new student array to local storage, or overwrite the old one.
+        //Local storage requires objects and arrays to be "stringified."
+        //We can parse them out when we need to.
+        window.localStorage.setItem("studentArr", JSON.stringify(studentArr));
 
-        findStudents();
-
+        //Add just the most recent student to the page.
+        addNewStudent();
     }
 
+    //Let the user know they left something blank.
     else{
         alert("You left something blank!");
     }
 
 })
 
-function findStudents(){
 
-    if(window.localStorage.getItem("student") != null){
+function populateStudents(){
 
-        var student = JSON.parse(window.localStorage.getItem('student'));
-    
-        var list = document.querySelector("ul");
-        var entry = document.createElement("li");
-        var text = document.createTextNode("Name: " + student.firstName + " " + student.lastName + ", ID: " + student.id + ", Age: " + student.age + ", Major:" + student.degree)
-        entry.appendChild(text);
-        list.appendChild(entry);
+    //Check if there is a student array in storage.
+    if(window.localStorage.getItem("studentArr")){
+        
+        //If there is an array of students, pull it from storage, parse the data, and place it in a variable.
+        var getStudentArr = JSON.parse(window.localStorage.getItem('studentArr'));
+        
+        //Go through each object in the array, and use its data to create a new list item.
+        for(var i = 0; i < getStudentArr.length; i++){
 
+            var list = document.querySelector("ul");
+            var entry = document.createElement("li");
+
+            var delButton = document.createElement("button")
+            delButton.className = "delButton";
+            delButton.id = getStudentArr[i].id;
+            delButton.innerHTML = "Delete"
+
+            var text = document.createTextNode("Name: " + getStudentArr[i].firstName + " " + getStudentArr[i].lastName 
+                                                + ", ID: " + getStudentArr[i].id 
+                                                + ", Age: " + getStudentArr[i].age 
+                                                + ", Major:" + getStudentArr[i].degree + " ")
+
+            text.iD = getStudentArr[i].id;
+
+            entry.appendChild(text);
+            entry.appendChild(delButton);
+            list.appendChild(entry);
+
+            addDelete();
+
+        }
     }
 }
+
+function addNewStudent(){
+
+    //Functions similar to findStudents, but we only want the object at the last index of the array.
+
+    if(window.localStorage.getItem("studentArr")){
+
+        var getStudentArr = JSON.parse(window.localStorage.getItem('studentArr'));
+        
+        var i = getStudentArr.length-1;
+
+        var list = document.querySelector("ul");
+        var entry = document.createElement("li");
+
+        var delButton = document.createElement("button")
+        delButton.className = "delButton";
+        delButton.id = getStudentArr[i].id;
+        delButton.innerHTML = "Delete"
+
+        var text = document.createTextNode("Name: " + getStudentArr[i].firstName + " " + getStudentArr[i].lastName
+            + ", ID: " + getStudentArr[i].id
+            + ", Age: " + getStudentArr[i].age
+            + ", Major:" + getStudentArr[i].degree + " ")
+
+        text.iD = getStudentArr[i].id;
+
+        entry.appendChild(text);
+        entry.appendChild(delButton);
+        list.appendChild(entry);
+
+        addDelete();
+    }
+
+}
+
+function addDelete(){
+
+    var delButtons = document.getElementsByClassName("delButton");
+
+    for(var i = 0; i < delButtons.length; i++){
+
+        delButtons[i].addEventListener("click", function() {
+    
+            var deleteID = this.id;
+            var badStudentInd;
+            studentArr = JSON.parse(window.localStorage.getItem("studentArr"));
+    
+            for( var k = 0; k < studentArr.length; k++){
+    
+                if(studentArr[k].id == deleteID){
+    
+                    badStudentInd = k;
+    
+                }
+    
+            }
+    
+            console.log(badStudentInd);
+
+            deleteStudent(badStudentInd);
+            
+        });
+    
+    }
+
+}
+
+function deleteStudent(index){
+
+    studentArr = JSON.parse(window.localStorage.getItem("studentArr"));
+    studentArr.splice(index, index+1);
+    window.localStorage.setItem("studentArr", studentArr);
+    //repopulateStudents();
+
+}
+
+function repopulateStudents(){
+
+   var studentList = document.getElementsByTagName("li");
+
+   for(var x = 0; x < studentList.length; x++){
+
+        studentList[x].remove();
+
+   }
+
+   //addOldStudents();
+
+
+}
+
